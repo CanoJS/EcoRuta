a75e204e1ef2453f84c57be21998463b60341306
CanoJS <jesuscanomendez91@gmail.com>
2026-04-09 19:16:03 -0600

    Home listo

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import Recompensas from "./pages/Recompensas";
 
 function App() {
   const [page, setPage] = useState("home");
-
+  const [puntos, setPuntos] = useState(0);
   if (page === "home") return <Home setPage={setPage} />;
   if (page === "login") return <Login setPage={setPage} />;
-  if (page === "dashboard") return <Dashboard setPage={setPage} />;
-  if (page === "recompensas") return <Recompensas setPage={setPage} />;
+  if (page === "dashboard") return <Dashboard setPage={setPage} puntos={puntos} setPuntos={setPuntos} />;
+  if (page === "recompensas") return <Recompensas setPage={setPage} puntos={puntos} setPuntos={setPuntos} />;
 
   return null;
 }
